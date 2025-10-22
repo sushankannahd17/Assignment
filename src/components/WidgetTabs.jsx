1d7f210349fe6376efca406d7fe43f25499c47d3
Sushan Kannah D <sushan.kd17@gmail.com>
2025-10-22 17:13:04 +0530

    Added text

--- src/components/WidgetTabs.jsx
+++ src/components/WidgetTabs.jsx
@@ -22,7 +22,9 @@ export default function WidgetTabs() {
         <div className='h-8 w-8 rounded-full badge flex items-center justify-center text-sm text-muted select-none'>?</div>
       </div>
       <div className='h-48 overflow-auto scrollbar-thin pr-2'>
-        {active === 0 && <div className='text-sm leading-7 text-gray-200'><p>Hello! I'm Dave, your sales rep here from Salesforce.</p></div>}
+        {active === 0 && <div className='text-sm leading-7 text-gray-200'><p>Hello! I’m Dave, your sales rep here from Salesforce. I’ve been working at this awesome company for 3 years now. <br /> <br /> 
+
+I was born and raised in Albany, NY& have been living in Santa Carla for the past 10 years my wife Tiffany and my 4 year old twin daughters- Emma and Ella. Both of them are just starting school, so my calender is usually blocked between 9-10 AM. This is a...</p></div>}
         {active === 1 && <div className='text-sm leading-7 text-gray-200'><p>Experience details go here.</p></div>}
         {active === 2 && <div className='text-sm leading-7 text-gray-200'><p>Recommended section content.</p></div>}
       </div>
